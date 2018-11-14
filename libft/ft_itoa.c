b9d9c77b2c28e84af16d937a270e790e499f27e8
Mathilde BONATI <mbonati@e3r10p15.42.fr>
2018-11-14 20:01:47 +0100

    libft

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mbonati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 22:44:00 by mbonati           #+#    #+#             */
-/*   Updated: 2018/11/13 22:45:19 by mbonati          ###   ########.fr       */
+/*   Updated: 2018/11/14 17:25:41 by mbonati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,36 @@
 
 char	*ft_itoa(int n)
 {
+	int size;
+	int i;
+	char *result;
+	int puissance;
 
+	size = ft_intsize(n);
+	if (!(result = malloc(sizeof(char) * (size + 1))))
+			return (NULL);
+	i = 0;
+	if (n < 0)
+	{
+		result[i] = '-';
+		n = -n;
+		i++;
+		size--;
+	}
+	puissance = 1;
+	while (size > 1)
+	{
+		puissance *= 10;
+		size--;
+	}
+	while (puissance > 1)
+	{
+		result[i] = n / puissance + '0';
+		n -= (n / puissance) * puissance;
+		puissance /= 10;
+		i++;
+	}
+	result[i] = n + '0';
+	result[i + 1] = '\0';
+	return (result);
 }
--- libft/ft_memalloc.c
+++ libft/ft_memalloc.c
@@ -1,6 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbonati <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/14 17:35:11 by mbonati           #+#    #+#             */
+/*   Updated: 2018/11/14 17:40:29 by mbonati          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	ft_memalloc(size_t size)
+void	*ft_memalloc(size_t size)
 {
-	
+	void	*tab;
+	if(!(tab = malloc(sizeof(void) * size)))
+		return (0);
+	while (size > 0)
+	{
+		tab[size] = 0;
+		size--;
+	}
+	return (tab);
 }
