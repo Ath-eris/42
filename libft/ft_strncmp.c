cd7835ed57d3baae47765acc629b9b616c302c50
Mathilde BONATI <mbonati@e3r10p15.42.fr>
2018-11-19 18:22:18 +0100

    libft

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mbonati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 14:18:58 by mbonati           #+#    #+#             */
-/*   Updated: 2018/11/19 14:48:40 by mbonati          ###   ########.fr       */
+/*   Updated: 2018/11/19 18:12:46 by mbonati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
--- libft/ft_strnequ.c
+++ libft/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: mbonati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 17:56:58 by mbonati           #+#    #+#             */
-/*   Updated: 2018/11/16 19:21:56 by mbonati          ###   ########.fr       */
+/*   Updated: 2018/11/19 18:20:07 by mbonati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while (s1[i] && s2[i] && i < n - 1)
 	{
 		if (s1[i] != s2[i])
 			return (0);
