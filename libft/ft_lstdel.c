9b867b035c5958e2c48d1c2f04078cdb7baeda01
Mathilde BONATI <mbonati@e3r12p22.42.fr>
2018-11-22 20:07:03 +0100

    libft

--- libft/ft_lstdel.c
+++ libft/ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: mbonati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 14:31:08 by mbonati           #+#    #+#             */
-/*   Updated: 2018/11/22 19:05:09 by mbonati          ###   ########.fr       */
+/*   Updated: 2018/11/22 20:00:23 by mbonati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	while ((*alst)->next != NULL)
+	t_list	*nxt_tmp;
+
+	nxt_tmp = (*alst)->next;
+	while (nxt_tmp != NULL)
 	{
+		nxt_tmp = (*alst)->next;
 		del((*alst)->content, (*alst)->content_size);
-		*alst = (*alst)->next;
+		free(*alst);
+		*alst = nxt_tmp;
 	}
-	free(*alst);
-	*alst = NULL;
 }
