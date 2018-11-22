51465b7e69eab1b4edefd9e7b0df6e068918eb8c
Mathilde BONATI <mbonati@e3r12p22.42.fr>
2018-11-22 18:17:57 +0100

    libft

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,10 +6,11 @@
 /*   By: mbonati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 12:58:53 by mbonati           #+#    #+#             */
-/*   Updated: 2018/11/21 14:58:17 by mbonati          ###   ########.fr       */
+/*   Updated: 2018/11/22 18:16:31 by mbonati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "libft.h"
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
@@ -24,10 +25,8 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 		list->content_size = 0;
 		return (list);
 	}
-	if (!(list->content = malloc(sizeof(content_size))))
-		return (NULL);
+	list->content = ft_strndup((char *)content, content_size);
 	list->next = NULL;
-	list->content = (void *)content;
 	list->content_size = content_size;
 	return (list);
 }
