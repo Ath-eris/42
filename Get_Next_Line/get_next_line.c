fc9c5668a2a2c9ee95eab1076e74533c63f9a4b2
Ath-eris <Atheris_@outlook.fr>
2018-12-03 22:51:27 +0100

    GNL

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbonati <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/28 17:05:53 by mbonati           #+#    #+#             */
-/*   Updated: 2018/12/03 19:20:19 by mbonati          ###   ########.fr       */
+/*   Created: 2018/12/03 22:06:28 by mbonati           #+#    #+#             */
+/*   Updated: 2018/12/03 22:51:09 by mbonati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,56 +15,41 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "libft/libft.h"
-#include <stdio.h>
 
-void	buf_split(char *buf, char **line)
+static int	buf_split(char *buf, char **line)
 {
-	size_t	i;
+	char	*endl_ptr;
 
-	i = 0;
-	while (buf[i] != '\n')
-		i++;
-	buf[i] = '\0';
+	if (buf[0] == '\0')
+		return (1);
+	if ((endl_ptr = ft_strchr(buf, '\n')))
+		*endl_ptr = '\0';
 	if (*line == NULL)
 		*line = ft_strdup(buf);
 	else
-		*line = ft_strjoin(*line, buf);
-	i++;
-	ft_strcpy(buf, buf + i);
+		*line = ft_strjoinfree(*line, buf, 1);
+	if (endl_ptr)
+		ft_strcpy(buf, endl_ptr + 1);
+	return (endl_ptr == NULL);
 }
 
-int		get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
-	int					ret_read;
-	static char			*buf = NULL;
+	int				ret_read;
+	static char		*buf = NULL;
 
 	*line = NULL;
 	if (buf == NULL)
 		if (!(buf = ft_memalloc(BUFF_SIZE + 1)))
 			return (-1);
-	if (ft_strchr(buf, '\n') != NULL)
-		buf_split(buf, line);
-	if (buf[0] != '\0')
-		*line = ft_strdup(buf);
-	if (buf[0] == '\0')
+	while (buf_split(buf, line))
 	{
 		if (!(ret_read = read(fd, buf, BUFF_SIZE)))
-			return (0);
-	}
-	else
-	{
-		ret_read = read(fd, buf, BUFF_SIZE);
-		buf[ret_read] = '\0';
-		while (ft_strchr(buf, '\n') == NULL)
-		{
 			if (*line == NULL)
-				*line = ft_strdup(buf);
-			else
-				*line = ft_strjoin(*line, buf);
-			ret_read = read(fd, buf, BUFF_SIZE);
-			buf[ret_read] = '\0';
-		}
-		buf_split(buf, line);
+				return (0);
+		if (ret_read == -1)
+			return (-1);
+		buf[ret_read] = '\0';
 	}
 	return (1);
 }
